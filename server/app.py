79067c9b83c5afa639b8bbd0025ce6b2f6935b8c
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-16 12:52:06 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from models import db, Article, User
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -17,6 +17,40 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+class Login(Resource):
+    def post(self):
+        data=request.get_json()
+        username= data.get('username')
+
+        if not username:
+            return {'maessage': 'Username is required'},400
+        
+        user=User.query.filter_by(username=username).first()
+
+        if not user:
+            return {'message':'User not found'},404
+        
+        session['user_id']=user.id
+
+        return jsonify(user.to_dict()),200
+    
+
+
+class Logout(Resource):
+     def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            return jsonify(user.to_dict()), 200
+
+        return {}, 401
 
 class ClearSession(Resource):
 
@@ -48,6 +82,10 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
